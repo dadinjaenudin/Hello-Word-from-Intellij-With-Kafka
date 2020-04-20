ce8af28df9eab5e5219cd899d3ab418e9f61b5e4
dadinjaenudin <jaenudin@gmail.com>
2020-04-20 03:12:00 -0700

    Initial commit

---
+++ src/main/java/org/djsoft/kafka/learning/common/AppConfigs.java
@@ -0,0 +1,10 @@
+package org.djsoft.kafka.learning.common;
+
+public class AppConfigs {
+
+    public final static String applicationID = "HelloKafkaProducer";
+    //public final static String bootstrapServers = "localhost:9092,localhost:9093";
+    public final static String bootstrapServers = "localhost:9092";
+    public final static String topicName = "testTopic";
+    public final static int numEvents = 10;
+}
